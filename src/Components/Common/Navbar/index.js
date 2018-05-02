4c9a6e3b32cce0552f9e217718119f04fc7b9efe
luoxiazx <1958745740@qq.com>
2018-05-02 15:43:12 +0800

    navbar1.0

--- src/Components/Common/Navbar/index.js
+++ src/Components/Common/Navbar/index.js
@@ -1,11 +1,21 @@
 import React,{Component}from 'react';
+import {NavLink} from "react-router-dom";
 import './index.css';
 
 class Navbar extends Component{
 	render(){
 		return(
 			<div>
-				Navbar
+				<header>
+					<ul className="list">
+						<li><img src="http://static1.mtime.cn/html5/20180426170931/images/2014/logo_mtime.png"/></li>
+						<li><NavLink to="/home" activeClassName="active">首页<div></div></NavLink></li>
+						<li><NavLink to="/list" activeClassName="active">购票<div></div></NavLink></li>
+						<li><NavLink to="/shop" activeClassName="active">商城<div></div></NavLink></li>
+						<li><NavLink to="/find" activeClassName="active">发现<div></div></NavLink></li>
+						<li><i className="iconfont icon-account"></i></li>
+					</ul>
+				</header>
 			</div>
 			);
 	}
