940b121a623a123531cae0509587270ba97a3df1
Infinitx <37289102+Infinitx@users.noreply.github.com>
2018-05-02 20:54:55 +0800

    Add files via upload

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,11 +1,19 @@
 import React,{Component}from 'react';
 import './index.css';
-
+import {NavLink} from 'react-router-dom';
+import axios from "axios";
 class Home extends Component{
+
+	componentDidMount(){
+		axios.get('/Service/callback.mi/Showtime/LocationMovies.api?locationId=290&t=20185217173147942').then(res=>{
+			console.log(res.data)
+		})
+	}
+
 	render(){
 		return(
 			<div>
-				home
+				正在热映
 			</div>
 			);
 	}
