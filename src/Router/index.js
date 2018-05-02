607ce46a5765ce5235b3f2615847bb6bcc00e91b
luoxiazx <1958745740@qq.com>
2018-05-02 16:55:56 +0800

    uprouter

--- src/Router/index.js
+++ src/Router/index.js
@@ -8,6 +8,8 @@ import React from 'react'
 import Home from '../Components/Home'
 import Film from '../Components/Film'
 import List from '../Components/List'
+import Shop from '../Components/Shop'
+import Find from '../Components/Find'
 import App from '../Components/App'
 import Detail from '../Components/Detail'
 const router=(
@@ -15,9 +17,11 @@ const router=(
 		<App>
 			<Switch>
 				<Route path="/home" component={Home}/>
-				<Route path="/film" component={Film}/>	
-				<Route path="/detail" component={Detail}/>	
+				<Route path="/film" component={Film}/>
+				<Route path="/detail" component={Detail}/>
 				<Route path="/list" component={List}/>
+				<Route path="/shop" component={Shop}/>
+				<Route path="/find" component={Find}/>
 				<Redirect from="/" to="/home"/>
 			</Switch>
 		</App>
