296a2bcde282bb4d97bd5e40e2ac8070f4adbeac
luoxiazx <1958745740@qq.com>
2018-05-02 15:54:42 +0800

    upshop

---
+++ src/Components/Shop/index.js
@@ -0,0 +1,14 @@
+import React,{Component}from 'react';
+import './index.css';
+
+class List extends Component{
+	render(){
+		return(
+			<div>
+				Shop
+			</div>
+			);
+	}
+}
+
+export default List;
